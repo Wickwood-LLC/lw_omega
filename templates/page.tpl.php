f83ba3d7e44fe0a41c01467260be5513ef203a2d
palagpatski <palagpatski@gmail.com>
2018-09-28 15:24:06 +0800

    Add inner branding and styles

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -63,22 +63,24 @@
     <div class="l-page-inner">
         <header class="l-header" role="banner">
             <div class="l-branding">
-                <div class="site-brand">
-                    <?php if ($logo): ?>
-                    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-                    <?php endif; ?>
-                    <?php if ($site_name || $site_slogan): ?>
-                    <?php if ($site_name): ?>
-                    <h1 class="site-name">
-                                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-                              </h1>
-                    <?php endif; ?>
-                    <?php if ($site_slogan): ?>
-                    <h2 class="site-slogan"><?php print $site_slogan; ?></h2>
-                    <?php endif; ?>
-                    <?php endif; ?>
+                <div class="l-branding-inner">
+                    <div class="site-brand">
+                            <?php if ($logo): ?>
+                            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+                            <?php endif; ?>
+                            <?php if ($site_name || $site_slogan): ?>
+                            <?php if ($site_name): ?>
+                            <h1 class="site-name">
+                                        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+                                      </h1>
+                            <?php endif; ?>
+                            <?php if ($site_slogan): ?>
+                            <h2 class="site-slogan"><?php print $site_slogan; ?></h2>
+                            <?php endif; ?>
+                            <?php endif; ?>
+                    </div>
+                    <?php print render($page['branding']); ?>
                 </div>
-                <?php print render($page['branding']); ?>
             </div>
             <?php print render($page['header']); ?>
             <?php print render($page['navigation']); ?>
