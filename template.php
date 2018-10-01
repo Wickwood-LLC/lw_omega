3307de5a80476bc4ebce2b5a7ad982caee91d82d
palagpatski <palagpatski@gmail.com>
2018-10-01 16:23:14 +0800

    Breadcrumb theming

--- template.php
+++ template.php
@@ -9,12 +9,12 @@
 function lw_omega_breadcrumb($variables) {
   $breadcrumb = $variables['breadcrumb'];
 
-  if (!empty($breadcrumb) && (count($breadcrumb) > 2 )) {
+  if (!empty($breadcrumb) && (count($breadcrumb) > 2 )) {	// Show only if there are more than 2 links
     // Provide a navigational heading to give context for breadcrumb links to
     // screen-reader users. Make the heading invisible with .element-invisible.
-    $output = '<h2 class="element-invisible art-postheader">' . t('You are here') . '</h2>';
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
 
-    $output .= '<div class="breadcrumb art-postcontent">' . implode(' » ', $breadcrumb) . '</div>';
+    $output .= '<div class="breadcrumb">' . implode(' » ', $breadcrumb) . '</div>';
     return $output;
   }
 }
