d04c9f7d2b93e6e65a82c1fa9c33b07bc3654cd4
palagpatski <palagpatski@gmail.com>
2018-11-23 14:25:33 +0800

    Add separators to node links

--- template.php
+++ template.php
@@ -71,3 +71,86 @@ function lw_omega_views_pre_render(&$view)
         drupal_add_css(drupal_get_path('theme', 'lw_omega') . '/css/published-articles-view.css', array('group' => CSS_THEME));
     }
 }
+
+function lw_omega_links($variables) {
+  $links = (array) $variables['links'];
+  $attributes = (array) $variables['attributes'];
+  $heading = $variables['heading'];
+  global $language_url;
+  $output = '';
+  if (!empty($links)) {
+
+    // Treat the heading first if it is present to prepend it to the
+    // list of links.
+    if (!empty($heading)) {
+      if (is_string($heading)) {
+
+        // Prepare the array that will be used when the passed heading
+        // is a string.
+        $heading = array(
+          'text' => $heading,
+          // Set the default level of the heading.
+          'level' => 'h2',
+        );
+      }
+      $output .= '<' . $heading['level'];
+      if (!empty($heading['class'])) {
+        $output .= drupal_attributes(array(
+          'class' => $heading['class'],
+        ));
+      }
+      $output .= '>' . check_plain($heading['text']) . '</' . $heading['level'] . '>';
+    }
+    $output .= '<ul' . drupal_attributes($attributes) . '>';
+    $num_links = count($links);
+    $i = 1;
+    foreach ($links as $key => $link) {
+      $class = array(
+        $key,
+      );
+
+      // Add first, last and active classes to the list of links to help out
+      // themers.
+      if ($i == 1) {
+        $class[] = 'first';
+      }
+      if ($i == $num_links) {
+        $class[] = 'last';
+      }
+      if (isset($link['href']) && ($link['href'] == $_GET['q'] || $link['href'] == '<front>' && drupal_is_front_page()) && (empty($link['language']) || $link['language']->language == $language_url->language)) {
+        $class[] = 'active';
+      }
+      $output .= '<li' . drupal_attributes(array(
+        'class' => $class,
+      )) . '>';
+      if (isset($link['href'])) {
+
+        // Pass in $link as $options, they share the same keys.
+        $output .= l($link['title'], $link['href'], $link);
+      }
+      elseif (!empty($link['title'])) {
+
+        // Some links are actually not links, but we wrap these in <span> for
+        // adding title and class attributes.
+        if (empty($link['html'])) {
+          $link['title'] = check_plain($link['title']);
+        }
+        $span_attributes = '';
+        if (isset($link['attributes'])) {
+          $span_attributes = drupal_attributes($link['attributes']);
+        }
+        $output .= '<span' . $span_attributes . '>' . $link['title'] . '</span>';
+      }
+      if ($i != $num_links) {
+        $output .= "</li> | \n";  
+      }
+      else {
+        $output .= "</li>\n";
+      }
+      $i++;
+    }
+    $output .= '</ul>';
+  }
+  return $output;
+}
+
